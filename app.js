2220d2cb64f0264ef228bc7e19aa6bb6962d2ab9
Mark Batho <mark747488@gmail.com>
2022-11-02 14:52:16 +0100

    Implement hovering over squares

--- app.js
+++ app.js
@@ -7,9 +7,13 @@ function generateSquareGrid(count, width, container) {
   for (let i = 0; i < count; i++) {
     for (let j = 0; j < count; j++) {
       const square = document.createElement('div');
+      square.classList.add('square');
       square.style.width = width;
       square.style.height = width;
-      square.style.backgroundColor = '#ccc';
+      square.addEventListener('mouseover', function () {
+        this.classList.add('hovered-square');
+        console.log(this);
+      });
 
       container.style.gridTemplateColumns = `repeat(${count}, auto)`;
       container.style.gridTemplateRows = `repeat(${count}, auto)`;
